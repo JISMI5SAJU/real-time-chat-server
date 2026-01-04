228eec97c5529b1772eb076a135bcbc1504c7f76
Octopusjippy <jismi949@gmail.com>
2026-01-04 11:48:30 +0530

    Initial commit: real-time chat backend with Socket.IO

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+
+// Serve static HTML files
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    io.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
